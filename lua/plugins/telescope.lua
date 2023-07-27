d3506c66367e5f99d4224629ac0bbe1deb50bb66
panditraghav <panditraghav18@gmail.com>
2023-07-27 12:12:31 +0530

    increased width of telescope preview

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    opts = {
+        defaults = {
+            layout_strategy = 'horizontal',
+            layout_config = { preview_width = 0.55, width = 0.95 },
+        }
+    }
+}
